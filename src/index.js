9da0df6e3cc1adb856ef638e42bb184b3b8f48a0
Poorjoy <madison_shepard@mail.ru>
2020-02-27 22:44:41 +0300

    feat: typical-arrays-problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array) {
+        return 0;
+    }
+    return array.length ? Math.min.apply(this, array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array) {
+        return 0;
+    }
+    return array.length ? Math.max.apply(this, array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array) {
+        return 0;
+    }
+    return array.length ? array.reduce((a, c) => a + c) / array.length : 0;
 }
